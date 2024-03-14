e6ad2e8e0421c4407f9c70c4c5271eda7c055ae3
Andrés Mahindo <mcandres@iesrafaelalberti.es>
2024-03-14 18:57:11 +0100

    primer commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 package org.pebiblioteca
 
 fun main() {
-    println("Hello World!")
+
+    val biblioteca = GestorBiblioteca()
+
+    val libro1 = Libro("1","Sevilla", "España", 1492, "Historia")
+    val libro2 = Libro("2","Cádiz", "España", 2000, "Geografía")
+    val libro3 = Libro("3","Madrid", "España", 1492, "Economía")
+
+    biblioteca.prestar(libro1)
 }
